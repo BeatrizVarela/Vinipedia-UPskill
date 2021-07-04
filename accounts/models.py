7daab744ae2733665cc2d5362c0f428c241f2804
Francisco Chicambi <antonio.chicambi-rocha@vinci-energies.net>
2021-07-04 23:53:12 +0100

    migrations birthday

--- accounts/models.py
+++ accounts/models.py
@@ -6,7 +6,7 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
     image = models.ImageField(upload_to='images/profiles/', blank=True)
     phone = models.CharField(max_length=10, blank=True)
-    birthday = models.DateField(null=True, blank=True)
+    birthday = models.DateField(default='1999-12-31', blank=True)
 
     def __str__(self):
         return f'{self.user.username} Profile'
